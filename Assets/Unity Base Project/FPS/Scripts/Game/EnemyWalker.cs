494d1e52d2cd067c378de5cf254a5a73ee458fa7
JaydenHollissSAE <159383304+JaydenHollissSAE@users.noreply.github.com>
2024-06-06 11:35:06 +1000

    Stalker Tracking Added

---
+++ Assets/Unity Base Project/FPS/Scripts/Game/EnemyWalker.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyWalker : SimpleBaseEnemy
+{
+    private GameObject playerObject;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        playerObject = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        float distance = Vector3.Distance(this.transform.position, playerObject.transform.position);
+        //Debug.Log(distance);
+        if (distance < 15.0f)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, playerObject.transform.position, Time.deltaTime * 5.5f);
+        }
+    }
+}
