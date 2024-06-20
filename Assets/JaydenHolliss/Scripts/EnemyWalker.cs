733016dc17bc7934a5ca5a2eb4eeaa7cd65832e6
JaydenHollissSAE <159383304+JaydenHollissSAE@users.noreply.github.com>
2024-06-20 13:23:17 +1000

    Set Walker damager to protect function

--- Assets/JaydenHolliss/Scripts/EnemyWalker.cs
+++ Assets/JaydenHolliss/Scripts/EnemyWalker.cs
@@ -9,6 +9,7 @@ public class EnemyWalker : SimpleBaseEnemy
 
     [SerializeField] float damage = 10.0f;
     bool damageBuffer = false;
+    private float distance;
 
     private void Start()
     {
@@ -18,7 +19,7 @@ public class EnemyWalker : SimpleBaseEnemy
     // Update is called once per frame
     private void Update()
     {
-        float distance = Vector3.Distance(this.transform.position, playerObject.transform.position); //Gets the distance of the enemy object from the player object.
+        distance = Vector3.Distance(this.transform.position, playerObject.transform.position); //Gets the distance of the enemy object from the player object.
         //Debug.Log(distance); //Prints the distance from the player object in the console for debigging.
         if (distance < 15.0f) //Checks if the distance from the player object is below 15.
         {
@@ -29,14 +30,7 @@ public class EnemyWalker : SimpleBaseEnemy
 
         //Debug.Log(playerObject.transform.rotation); //Prints the player object's rotation to the console for debugging.
 
-        /// <summary>
-        /// Artifical Collision and Damage
-        /// </summary>
-        if (distance < 1.0f && !damageBuffer) //Checks if the distance from the player object is smaller than 1 and if damageBuffer is false.
-        {
-            playerObject.GetComponent<Health>().TakeDamage(damage, gameObject); //Inflicts damage to the player based on the damage variable.
-            StartCoroutine(DamagePause()); //Begins the DamagePause function to give a buffer between damage.
-        }
+        PerformDamage();
 
     }
 
@@ -45,5 +39,17 @@ public class EnemyWalker : SimpleBaseEnemy
         damageBuffer = true; //Sets damageBuffer to true to prevent the player from being damaged by the enemy.
         yield return new WaitForSeconds(0.8f); //Waits 0.8 seconds to act as a buffer between enemy attacks.
         damageBuffer = false; //Sets damageBuffer to false to allow the enemy to damage the player again.
+    }
+
+    protected void PerformDamage()
+    {
+        /// <summary>
+        /// Artifical Collision and Damage
+        /// </summary>
+        if (distance < 1.0f && !damageBuffer) //Checks if the distance from the player object is smaller than 1 and if damageBuffer is false.
+        {
+            playerObject.GetComponent<Health>().TakeDamage(damage, gameObject); //Inflicts damage to the player based on the damage variable.
+            StartCoroutine(DamagePause()); //Begins the DamagePause function to give a buffer between damage.
+        }
     }
 }
