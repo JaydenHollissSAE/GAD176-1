1f70ea9594c09735c41c3e6b850c1c2baa3da370
JaydenHollissSAE <159383304+JaydenHollissSAE@users.noreply.github.com>
2024-06-20 10:11:31 +1000

    Fixed Enemy Spawn locations

--- Assets/JaydenHolliss/Scripts/EnemySpawner.cs
+++ Assets/JaydenHolliss/Scripts/EnemySpawner.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    [SerializeField] float roomPos;
     [SerializeField] float varyDistance;
     private Vector3 spawnPos;
     private int spawnAmount;
     [SerializeField] List<GameObject> enemyList = new List<GameObject>();
     [SerializeField] int maxEnemies;
     [SerializeField] int minEnemies;
+    private int xSide;
+    private int zSide;
 
     private void Start()
     {
@@ -19,9 +20,31 @@ public class EnemySpawner : MonoBehaviour
         spawnAmount = Random.Range(minEnemies, maxEnemies);
         for (int i = 0; i < spawnAmount; i++)
         {
-            spawnPos = new Vector3(roomPos+Random.Range(1f, varyDistance), 0f,roomPos + Random.Range(1f, varyDistance)); //Gets a new random position for the coin to spawn on the map.
+
+            xSide = Random.Range(0, 100);
+            Debug.Log("xSide");
+            zSide = Random.Range(0, 100);
+            Debug.Log("zSide");
+            if (zSide < 50)
+            {
+                zSide = -1;
+            }
+            else
+            {
+                zSide = 1;
+            }
+            if (xSide < 50)
+            {
+                xSide = -1;
+            }
+            else
+            {
+                xSide = 1;
+            }
+            //Debug.Log("xSide is: "+xSide+" zSide is: "+zSide);
+            spawnPos = new Vector3(transform.position.x+(Random.Range(1f, varyDistance))*xSide, transform.position.y, transform.position.z + (Random.Range(1f, varyDistance))*zSide); //Gets a new random position for the coin to spawn on the map.
             Instantiate(enemyList[Random.Range(0,enemyList.Count)], spawnPos, Quaternion.identity);
-            Debug.Log("Enemy Spawned");
+            //Debug.Log("Enemy Spawned at: "+spawnPos);
         }
     }
 
